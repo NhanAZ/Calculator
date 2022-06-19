be82daea51e4160b1dabf02ab7ecaff889e97c24
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2022-06-19 13:47:53 +0700

    Exception handling

--- src/Main.php
+++ src/Main.php
@@ -10,15 +10,26 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
 
-	protected function onEnable() : void {
+	const PREFIX = "§f[§bCalculator§f]§r ";
+
+	protected function onEnable(): void {
 		$this->getLogger()->emergency("Absolutely do not use this plugin in production!");
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
 		if ($command->getName() === "calculator" && count($args) >= 1) {
 			$hanlder = implode("", $args);
-			$rerult = eval("return $hanlder;");
-			$sender->sendMessage("Result: " . number_format($rerult, 2));
+			var_dump($hanlder);
+			$this->getLogger()->alert("Executing PHP: $hanlder");
+			try {
+				$rerult = eval("return $hanlder;");
+				if (is_numeric($rerult)) {
+					$sender->sendMessage(self::PREFIX . "§aResult: " . number_format($rerult, 2));
+				}
+			} catch (\Throwable $e) {
+				$sender->sendMessage(self::PREFIX . "§cError: " . $e->getMessage());
+				return true;
+			}
 			return true;
 		}
 		return false;
